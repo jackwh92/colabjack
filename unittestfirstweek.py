cdfe32df4ef9db88868d6f5e7725f5fd68500ade
jackwh92 <jack.hollies@digital.hmrc.gov.uk>
2019-01-03 14:36:50 +0000

    not sure

---
+++ unittestfirstweek.py
@@ -0,0 +1,11 @@
+import unittest
+from firstweek import function
+
+class theclass(unittest.TestCpythonase):
+
+    def test_totestaaddsb(self):
+        self.assertEqual(function(2,3), (5))
+
+if __name__=="__main__":
+    unittest.main()
+    
